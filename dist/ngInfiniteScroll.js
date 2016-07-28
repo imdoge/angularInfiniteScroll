700e63c553eed030ca80540854158525c9775f22
imdoge <imdoge2015@gmail.com>
2016-07-28 15:12:15 +0800

    Update ngInfiniteScroll.js

--- dist/ngInfiniteScroll.js
+++ dist/ngInfiniteScroll.js
@@ -1,89 +1,63 @@
 angular.module('ngInfiniteScroll', [])
-    .controller('InfiniteScrollCtrl', [
-        '$scope',
-        '$attrs',
-        '$element',
-        function($scope, $attrs, $element) {
-            var self = this,
-                container = $element[0];
-            
-            this.isLoading = false;
-            this.checkBounds = throttle(checkInfiniteBounds, +$attrs.wait || 300);
+    .directive('infiniteScroll', ['$window', '$timeout', function ($window, $timeout) {
+        return {
+            link: function (scope, element, attrs) {
+                var windowEl = angular.element($window),
+                    bindToWindow = scope.$eval(attrs.window),
+                    elm = bindToWindow ? windowEl : element,
+                    container = bindToWindow ? document.body : element[0],
+                    checkBounds = throttle(checkInfiniteBounds, +attrs.wait || 300);
+                
+                elm.on('scroll', checkBounds);
+                
+                scope.$on('$destroy', function() {
+                    elm.off('scroll', checkBounds);
+                });
 
-            $scope.$on('infiniteScrollComplete', function() {
-                self.isLoading = false;
-            });
-
-            $scope.$on('$destroy', function() {
-                $element.off('scroll', self.checkBounds);
-            });
-            
-            function onInfinite() {
-                self.isLoading = true;
-                $scope.$apply($attrs.infiniteScroll || '');
-            }
-
-            function throttle(fn, wait) {
-                var last,
-                    deferTimer;
-                return function () {
-                    var context = this;
-
-                    var now = +new Date(),
-                        args = arguments;
-                    if (last && now < last + wait) {
-                        clearTimeout(deferTimer);
-                        deferTimer = setTimeout(function () {
+                if (attrs.immediateCheck != null && scope.$eval(attrs.immediateCheck)) {
+                    $timeout(function () { checkBounds(); });
+                }
+                
+                function throttle(fn, wait) {
+                    var last,
+                        deferTimer;
+                    return function () {
+                        var context = this;
+    
+                        var now = +new Date(),
+                            args = arguments;
+                        if (last && now < last + wait) {
+                            clearTimeout(deferTimer);
+                            deferTimer = setTimeout(function () {
+                                last = now;
+                                fn.apply(context, args);
+                            }, wait);
+                        } else {
                             last = now;
                             fn.apply(context, args);
-                        }, wait);
-                    } else {
-                        last = now;
-                        fn.apply(context, args);
+                        }
+                    };
+                }
+    
+                function checkInfiniteBounds() {
+                    var disabled = scope.$eval(attrs.infiniteScrollDisabled);
+                    if (disabled) { return; }
+                    
+                    var maxScroll = getMaxScroll(container.scrollHeight);
+                    var clientHeight = bindToWindow ? window.innerHeight || document.documentElement.clientHeight : container.clientHeight;
+                    
+                    if (container.scrollTop + clientHeight >= maxScroll) {
+                        scope.$apply(attrs.infiniteScroll || '');
                     }
-                };
-            }
-
-            function checkInfiniteBounds() {
-                var disabled = $scope.$eval($attrs.infiniteScrollDisabled);
-                if (self.isLoading || disabled) { return; }
-                var maxScroll = getMaxScroll();
-
-                if (maxScroll !== -1 && container.scrollTop >= maxScroll - container.clientHeight) {
-                    onInfinite();
                 }
-            }
-
-            function getMaxScroll() {
-                var computedStyle = window.getComputedStyle(container) || {};
-
-                return container.scrollHeight &&
-                        (computedStyle.overflowY === 'scroll' ||
-                        computedStyle.overflowY === 'auto' ||
-                        container.style['overflow-y'] === 'scroll' )
-                        ? calculateMaxValue(container.scrollHeight)
-                        : -1;
-            }
-
-            function calculateMaxValue(maximum) {
-                var distance = ($attrs.distance || '2.5%').trim();
-                var isPercent = distance.indexOf('%') > -1;
-                return isPercent
-                        ? maximum * (1 - parseFloat(distance) / 100)
-                        : maximum - parseFloat(distance);
-            }
-        }])
-    .directive('infiniteScroll', ['$timeout', function ($timeout) {
-        return {
-            require: 'infiniteScroll',
-            controller: 'InfiniteScrollCtrl',
-            link: function (scope, element, attrs, ctrl) {
-                element.on('scroll', ctrl.checkBounds);
-
-                if (attrs.immediateCheck != null) {
-                    var immediateCheck = scope.$eval(attrs.immediateCheck);
+    
+                function getMaxScroll(maximum) {
+                    var distance = (attrs.distance || '2.5%').trim();
+                    var isPercent = distance.indexOf('%') > -1;
+                    return isPercent
+                            ? maximum * (1 - parseFloat(distance) / 100)
+                            : maximum - parseFloat(distance);
                 }
-                immediateCheck && $timeout(function () { ctrl.checkBounds(); });
             }
         }
     }]);
